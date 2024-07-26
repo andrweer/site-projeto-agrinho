1a59e77df00da21777d6530f4e96fdd1b0bbb8c1
Andrij <132073432+andrweer@users.noreply.github.com>
2024-07-26 02:39:23 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    const headerHeight = document.querySelector('header').offsetHeight;
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = event.target.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - headerHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
